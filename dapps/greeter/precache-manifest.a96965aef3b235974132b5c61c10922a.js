c53b1dabc8af1fe6395cf9972daf5c1a8ea38659
Smith <velvet.nanashi@gmail.com>
2021-12-24 13:30:48 +0900

    added dapps tutorial

---
+++ dapps/greeter/precache-manifest.a96965aef3b235974132b5c61c10922a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97173342d6545e490652",
+    "url": "/static/css/main.e46dc8a1.chunk.css"
+  },
+  {
+    "revision": "97173342d6545e490652",
+    "url": "/static/js/main.97173342.chunk.js"
+  },
+  {
+    "revision": "ca7c8955edddd8af1f36",
+    "url": "/static/js/1.ca7c8955.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c5fc0900afe77a02eee54b04677f143b",
+    "url": "/index.html"
+  }
+];
